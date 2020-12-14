5beee38ca3ea5c84a20188c8f97f46f0f114af63
AnonProductions <anonproductions@protonmail.com>
2020-12-14 17:05:06 +0530

    feat: ✅ Esc to close navbar on mobile

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./TheHeader.css";
 
 const TheHeader = () => {
   const [navOpen, setNavOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (ev) => {
+      if (ev.key === "Escape") {
+        setNavOpen(false);
+      }
+    });
+  }, []);
 
   return (
     <header className="header">
@@ -15,7 +23,10 @@ const TheHeader = () => {
         <li className="link">Log in</li>
         <li className="link cta">Sign up</li>
       </ul>
-      <div className="hamburger" onClick={() => setNavOpen(!navOpen)}>
+      <div
+        className="hamburger"
+        onClick={() => setNavOpen((navOpen) => !navOpen)}
+      >
         <span className="line"></span>
         <span className="line"></span>
         <span className="line"></span>
